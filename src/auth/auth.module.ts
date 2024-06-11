26e51f1571b2f74518dee69807d2a5023e6c6936
iradukundapaci <paifique@gmail.com>
2024-06-11 18:54:45 +0200

    importing auth module and service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { MailerModule } from 'src/mailer/mailer.module';
+import { MailerService } from 'src/mailer/mailer.service';
 
 @Module({
+  imports: [JwtModule.register({}), MailerModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, MailerService],
 })
 class AuthModule {}
 
