692295d8b1b09b5eb3c225d8f864b72facb0cd11
iradukundapaci <paifique@gmail.com>
2024-06-11 18:53:05 +0200

    adding verify route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto';
+import { SigninDto, signupDto } from './dto';
 
 @Controller('auth')
 class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
   @Post('signin')
-  signin(@Body() dto: AuthDto) {
+  signin(@Body() dto: SigninDto) {
     return this.authService.signin(dto);
   }
   @Post('signup')
-  signup(@Body() dto: AuthDto) {
+  signup(@Body() dto: signupDto) {
     return this.authService.signup(dto);
+  }
+
+  @Get('verify')
+  async verify(@Query('token') token: string) {
+    return this.authService.verifyEmail(token);
   }
 }
 
