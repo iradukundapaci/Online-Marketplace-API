9a33f3c00281139dadb926e00e7e86f1e383171c
iradukundapaci <paifique@gmail.com>
2024-06-11 19:07:52 +0200

    adding mail and prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,21 @@ import { OrderModule } from './order/order.module';
 import { CategoryModule } from './category/category.module';
 import { ReviewModule } from './review/review.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from './mailer/mailer.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     ProductModule,
     OrderModule,
     CategoryModule,
     ReviewModule,
     UserModule,
+    PrismaModule,
+    MailerModule,
   ],
 })
 export class AppModule {}
