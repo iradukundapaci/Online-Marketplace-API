0df86e57434096921dc30bab33fef4a8e8dbe943
iradukundapaci <paifique@gmail.com>
2024-06-13 08:15:14 +0200

    adding product CRUD operation

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { ProductService } from './product.service';
 
-@Controller('product')
-export class ProductController {}
+@Controller('products')
+export class ProductController {
+  constructor(private readonly productsService: ProductService) {}
+
+  @Post()
+  create(@Body() createProductDto: Prisma.ProductCreateInput) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: Prisma.ProductUpdateInput,
+  ) {
+    return this.productsService.update(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(id);
+  }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, Product } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: Prisma.ProductCreateInput): Promise<Product> {
+    return this.prisma.product.create({
+      data,
+    });
+  }
+
+  async findAll(): Promise<Product[]> {
+    return this.prisma.product.findMany();
+  }
+
+  async findOne(productId: number): Promise<Product> {
+    return this.prisma.product.findUnique({
+      where: { productId },
+    });
+  }
+
+  async update(
+    productId: number,
+    data: Prisma.ProductUpdateInput,
+  ): Promise<Product> {
+    return this.prisma.product.update({
+      where: { productId },
+      data,
+    });
+  }
+
+  async remove(productId: number): Promise<Product> {
+    return this.prisma.product.delete({
+      where: { productId },
+    });
+  }
+}
