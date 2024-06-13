5f8b089b73e3bd84060de9a072b6f6531f9e7068
iradukundapaci <paifique@gmail.com>
2024-06-13 08:51:35 +0200

    adding CRUD service logic

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma, User } from '@prisma/client';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
+
+  async findOne(userId: number): Promise<User> {
+    return this.prisma.user.findUnique({
+      where: { userId },
+    });
+  }
+
+  async update(userId: number, data: Prisma.UserUpdateInput): Promise<User> {
+    return this.prisma.user.update({
+      where: { userId },
+      data,
+    });
+  }
+
+  async remove(userId: number): Promise<User> {
+    return this.prisma.user.delete({
+      where: { userId },
+    });
+  }
+}
