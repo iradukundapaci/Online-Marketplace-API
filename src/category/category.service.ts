96e46195206bfeada4e3c2d13024fab4941bc668
iradukundapaci <paifique@gmail.com>
2024-06-13 08:21:56 +0200

    adding category CRUD services

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,41 @@
+// src/categories/categories.service.ts
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma, Category } from '@prisma/client';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: Prisma.CategoryCreateInput): Promise<Category> {
+    return this.prisma.category.create({
+      data,
+    });
+  }
+
+  async findAll(): Promise<Category[]> {
+    return this.prisma.category.findMany();
+  }
+
+  async findOne(categoryId: number): Promise<Category> {
+    return this.prisma.category.findUnique({
+      where: { categoryId },
+    });
+  }
+
+  async update(
+    categoryId: number,
+    data: Prisma.CategoryUpdateInput,
+  ): Promise<Category> {
+    return this.prisma.category.update({
+      where: { categoryId },
+      data,
+    });
+  }
+
+  async remove(categoryId: number): Promise<Category> {
+    return this.prisma.category.delete({
+      where: { categoryId },
+    });
+  }
+}
