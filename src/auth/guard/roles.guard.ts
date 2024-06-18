bfe7c7219188d354e1da37e54a88af19cea0e9ae
iradukundapaci <paifique@gmail.com>
2024-06-18 10:37:26 +0200

    removing loggers

--- src/auth/guard/roles.guard.ts
+++ src/auth/guard/roles.guard.ts
@@ -1,17 +1,10 @@
-import {
-  Injectable,
-  CanActivate,
-  ExecutionContext,
-  Logger,
-} from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from '@prisma/client';
 import { ROLES_KEY } from '../decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  private readonly logger = new Logger(RolesGuard.name);
-
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
@@ -23,8 +16,6 @@ export class RolesGuard implements CanActivate {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
-    this.logger.debug(`User Role: ${user.role}`);
-    this.logger.debug(`Required Roles: ${requiredRoles}`);
     return requiredRoles.includes(user.role);
   }
 }
